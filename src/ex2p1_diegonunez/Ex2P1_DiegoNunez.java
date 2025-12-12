424a2f233e9f538d316ab1c7d44ea0748d3eb658
Danifuzz <diego_nunez@unitec.edu>
2025-12-12 13:38:34 -0600

    Primer commit

---
+++ src/ex2p1_diegonunez/Ex2P1_DiegoNunez.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ex2p1_diegonunez;
+
+/**
+ *
+ * @author nunez
+ */
+public class Ex2P1_DiegoNunez {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
